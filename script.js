7034e737851ae5d98bdeb597cca1ae5adb23b5ff
sweetinmortal <asijerson@gmail.com>
2025-05-23 22:47:55 -0500

    Script creado, notas agregadas, calculo con pesos respectivos

---
+++ script.js
@@ -0,0 +1,7 @@
+// Generar tres notas aleatorias entre 0 y 20
+let parcial1 = 11;
+let parcial2 = 17;
+let proyectoFinal = 14;
+
+// Calcular la nota final con los pesos respectivos
+let notaFinal = (parcial1 * 0.3) + (parcial2 * 0.3) + (proyectoFinal * 0.4);
